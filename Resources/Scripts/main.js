df28361649448e128d752c544c9647165e4a4867
jpdzRamirez <jeremyivanpedraza@gmail.com>
2024-05-24 23:03:55 -0500

    terminal completed

--- Resources/Scripts/main.js
+++ Resources/Scripts/main.js
@@ -1,4 +1,6 @@
 
+const welcomeStartupXP=$('.welcome');
+const welcomeMenuDOS=$('.welcome-menu');
 
 // Start it up
 function clickTradicional() {
@@ -7,23 +9,32 @@ function clickTradicional() {
 }
 function clickModerno() {
     console.log('moderno');
-    
+    welcomeMenuDOS.addClass('exitLoading');
+    welcomeStartupXP.removeClass('hidden');
+    setTimeout(function() {
+        welcomeMenuDOS.remove();
+    }, 1500); 
+    startupXP();
 }
 function startupXP() {
     setTimeout(function() {
-        welcomeLoading.addClass('exitLoading');     
+        welcomeStartupXP.addClass('exitLoading');     
         setTimeout(function() {
-            welcomeLoading.remove();
-        }, 1000);
-    }, 3000);
+            welcomeStartupXP.remove();
+        }, 1500);
+    }, 6000);
 }
 $(function() {
    
     const thanks = $('.contact-gratitude');
     const menu = $('.main-menu');
+
     setTimeout(function() {
-        console.log('moderno');
         menu.removeAttr('hidden');
-        
-    }, 4000);
+        menu.addClass('shadow-animation');
+        setTimeout(function() {
+            thanks.removeClass('hidden');
+            thanks.addClass('shadow-animation');
+        },1000);
+    }, 15000);
 });
